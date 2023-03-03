8647e22506776af9aca493ed88dc9c3d42805a25
Ben Bright <nooc@users.noreply.github.com>
2023-03-03 14:28:59 +0100

    configurations

--- default/src/main/java/space/nixus/pubtrans/App.java
+++ default/src/main/java/space/nixus/pubtrans/App.java
@@ -1,7 +1,7 @@
 package space.nixus.pubtrans;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,7 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        var builder = new SpringApplicationBuilder(App.class);
+        builder.profiles("services");
+        // Set profiles depending on if GAE_ENV exists or not.
+        if(System.getenv("GAE_ENV")==null) {
+            builder.profiles("dev");
+        } else {
+            builder.profiles("prod");
+        }
+        builder.run(args);
     }
 
     @GetMapping("/swagger")
